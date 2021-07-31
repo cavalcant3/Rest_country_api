fb017a6d10b6884580c84521d630a2de82c57ef2
cavalcant3 <jmatheusoc269@gmail.com>
2021-07-31 14:12:33 -0300

    incial config

--- restcountry/src/App.js
+++ restcountry/src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const url = "https://restcountries.eu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Olá mundão</div>;
 }
 
 export default App;
