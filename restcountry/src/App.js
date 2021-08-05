82824d6af87faa4f366a185729752949af31c661
cavalcant3 <jmatheusoc269@gmail.com>
2021-08-05 15:29:15 -0300

    adicionado o filter e o campo de busca

--- restcountry/src/App.js
+++ restcountry/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Countries from "./components/Countries";
 import Header from "./components/Header";
+import Filter from "./components/Filter";
 
 function App() {
   return (
     <div>
       <Header />
+      <Filter />
       <Countries />
     </div>
   );
--- restcountry/src/components/Header.jsx
+++ restcountry/src/components/Header.jsx
@@ -7,6 +7,9 @@ const Header = () => {
         <div>
         <h1>Where in the world??</h1>
         </div>
+        <div>
+          <i className="moon"></i>Dark mode
+        </div>
       </header>
     </>
   )
